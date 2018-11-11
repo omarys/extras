36cc90a7bff0cde171967e01271f9786c4a1ac5c
Scott O'Mary <omaryscott@gmail.com>
2018-11-10 21:19:39 -0500

    wtf

--- setup_scripts/setup_ubuntu.sh
+++ setup_scripts/setup_ubuntu.sh
@@ -30,3 +30,10 @@ echo "downloading hack font from github"
 wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
 mkdir -p ~/.local/share/fonts && mv 'Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf' ~/.local/share/fonts
 
+echo "installing pip"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+
+pip install rtv --user
+pip install pipenv --user
+pip install ansible --user
