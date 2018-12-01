bdbbb74c283d503bebb93f0c0c4333fc2be79269
Scott O'Mary <omaryscott@gmail.com>
2018-11-30 20:59:33 -0500

    Small changes to solus bash script.

--- setup_scripts/setup_solus.sh
+++ setup_scripts/setup_solus.sh
@@ -4,8 +4,6 @@
 sudo eopkg up -y
 sudo eopkg it git fish neovim -y
 
-sudo eopkg it $(eopkg sr -N libreoffice | cut -d" " -f 1 | tr '\n' ' ')
-
 # chsh
 chsh -s /usr/bin/fish
 
@@ -14,7 +12,8 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Powerline Fonts
-cd ~/git
-git clone git@github.com:ryanoasis/nerd-fonts.git
-cd nerd-fonts/ && sh install.sh
+echo "downloading hack font from github"
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
+mkdir -p ~/.local/share/fonts && mv 'Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf' ~/.local/share/fonts
+
 
