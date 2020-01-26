f8f73f436d4a2e9112fae4b7fe8d43c517d903f2
Scott O'Mary <omaryscott@gmail.com>
2020-01-26 14:23:43 -0500

    Added mpv and rtv configs

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -13,22 +13,16 @@ c.aliases = {
     'yt': 'open -t youtube.com'
 }
 
-# Enable JavaScript.
-# Type: Bool
 config.set('content.javascript.enabled', True, 'file://*')
-
-# Enable JavaScript.
-# Type: Bool
 config.set('content.javascript.enabled', True, 'chrome://*/*')
-
-# Enable JavaScript.
-# Type: Bool
 config.set('content.javascript.enabled', True, 'qute://*/*')
 
-c.content.proxy = 'none'
-
 c.url.default_page = 'https://google.com'
-
 c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}'}
-
 c.url.start_pages = 'https://google.com'
+
+c.tabs.position = 'left'
+c.tabs.width = '8%'
+c.tabs.background = True
+
+config.load_autoconfig()
