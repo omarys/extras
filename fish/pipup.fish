5a5a24416420fc45fe59e88774087b16c4c6e292
Scott O'Mary <omaryscott@gmail.com>
2022-04-29 20:59:41 -0500

    Added update file for pip packages.

---
+++ fish/pipup.fish
@@ -0,0 +1,7 @@
+pip install --upgrade black              
+pip install --upgrade catt               
+pip install --upgrade neovim             
+pip install --upgrade pip                
+pip install --upgrade pynvim             
+pip install --upgrade tuir               
+pip install --upgrade youtube-dl         
