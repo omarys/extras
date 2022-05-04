c8a5c470edf59f6264e92d6782055c4c18168ae6
Scott O'Mary <omaryscott@gmail.com>
2022-05-04 10:07:35 -0500

    Added qmk to list of updated python packages.

--- fish/update_packages.fish
+++ fish/update_packages.fish
@@ -4,6 +4,7 @@ pip install --upgrade catt
 pip install --upgrade neovim             
 pip install --upgrade pip                
 pip install --upgrade pynvim             
+pip install --upgrade qmk
 pip install --upgrade tuir               
 pip install --upgrade youtube-dl         
 sudo npm -g upgrade neovim prettier
