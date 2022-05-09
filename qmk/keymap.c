0e97bbf3f841ecbf674a5c02ad03cbbf7ebc28c2
Scott O'Mary <omaryscott@gmail.com>
2022-05-09 12:35:52 -0500

    Updated keymap to toggle numpad on/off.

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -47,32 +47,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_LGUI, KC_LALT, MO(1),   LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR
+  KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_QUOT,
+  KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  KC_LCTL, KC_LGUI, KC_LALT, MO(1), LOWER, KC_SPC, KC_SPC, TT(2), KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F3  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 | Bksp |
+ * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Tab  |   *  |   @  |   ^  |   %  |      |      |   -  |   =  |   \  |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Caps |   $  |   #  |   @  |   !  |      |   [  |   (  |   )  |   ]  |      |Enter |
+ * | Caps |   $  |   #  |   @  |   !  |      |   [  |   (  |   )  |   ]  |      |  '   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
+ * | Shift|      |      |      |      |      | Home | PgDn | PgUp | End  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | OS   | LAlt | Del  |      |     F12     |      | Left | Down |  Up  | Rght |
+ * | RCtrl| F11  |CtrAlt| Del  |Lower |     F12     |Raise | Left | Down |  Up  | Rght |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_TRNS,
-  KC_TRNS, KC_ASTR, KC_AMPR, KC_CIRC, KC_PERC, KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_BSLS, KC_NO,   KC_NO,
-  KC_CAPS, KC_DLR,  KC_HASH, KC_AT,   KC_EXLM, KC_NO,   KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_NO,   KC_TRNS,
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_F12,  KC_F12,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_GRV,         KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_TRNS,
+  KC_TRNS,        KC_ASTR, KC_AMPR, KC_CIRC, KC_PERC, KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_BSLS, KC_NO,   KC_NO,
+  KC_CAPS,        KC_DLR,  KC_HASH, KC_AT,   KC_EXLM, KC_NO,   KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_NO,   KC_QUOT,
+  KC_TRNS,        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   KC_TRNS,
+  LCA_T(KC_RCTL), KC_F11,  KC_TRNS, KC_DEL,  KC_TRNS, KC_F12,  KC_F12,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Raise
@@ -89,32 +89,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSLS, KC_PAST, KC_PMNS, KC_PMNS,
-  KC_NO,   KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_7,    KC_8,    KC_9,    KC_PPLS,
-  KC_TRNS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO,   KC_4,    KC_5,    KC_6,    KC_PPLS,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_1,    KC_2,    KC_3,    KC_ENT,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,    KC_PDOT, KC_CALC, KC_ENT
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_PSLS, KC_PAST, KC_PMNS, KC_PMNS,
+  KC_NO,   KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_7,    KC_8,    KC_9,    KC_PPLS,
+  KC_TRNS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO, KC_NO,    KC_4,    KC_5,    KC_6,    KC_PPLS,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_1,    KC_2,    KC_3,    KC_ENT,
+  KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_TRNS, KC_SPC,  KC_SPC, KC_TRNS, KC_0,    KC_DOT,  KC_CALC, KC_ENT
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |      |      |      |      |      |      |      |TermOf|TermOn|      |      |KC_PWR|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Reset| Debug|      |      |      |      |TermOf|TermOn|      |      |  Del |
+ * |      | Reset| Debug|      |      |      |      |KC_BRU|KC_BRD|      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|BL_STE|      |      |      |      |
+ * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|RGB_TG|RGB_VI|RGB_VD|      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|      |      |      |      |BL_TOG|
+ * |      |Voice-|Voice+|Mus on|MusOff|MidiOn|MidOff|RGB_MD|      |      |      |KC_SLK|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |RGBMOD|BL_INC|BL_DEC|RGBTOG|
+ * |      |      |      |      |      |             |      |BL_TOG|BL_DEC|BL_INC|KC_NUM|
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_preonic_grid(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, BL_STEP, KC_NO,    KC_NO,   _______, _______,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______,  _______, _______, BL_TOGG,
-  _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD,  BL_INC,  BL_DEC,  RGB_TOG
+  _______, _______, _______, _______, _______, _______, _______, TERM_ON, TERM_OFF, _______, _______, KC_PWR,
+  _______, RESET,   DEBUG,   _______, _______, _______, _______, KC_BRIU, KC_BRID,  _______, _______, _______,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, RGB_TOG, RGB_VAI,  RGB_VAD, _______, _______,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  RGB_MOD, _______,  _______, _______, KC_SLCK,
+  _______, _______, _______, _______, _______, _______, _______, _______, BL_TOGG,  BL_DEC,  BL_INC,  KC_NUM
 )
 
 
@@ -122,53 +122,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-        // case QWERTY:
-        //   if (record->event.pressed) {
-        //     set_single_persistent_default_layer(_QWERTY);
-        //   }
-        //   return false;
-        //   break;
-        case LOWER:
-          if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case RAISE:
-          if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case BACKLIT:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-            #ifdef BACKLIGHT_ENABLE
-              backlight_step();
-            #endif
-            #ifdef RGBLIGHT_ENABLE
-              rgblight_step();
-            #endif
-            #ifdef __AVR__
-            writePinLow(E6);
-            #endif
-          } else {
-            unregister_code(KC_RSFT);
-            #ifdef __AVR__
-            writePinHigh(E6);
-            #endif
-          }
-          return false;
-          break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
+      return false;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+    case BACKLIT:
+      if (record->event.pressed) {
+        register_code(KC_RSFT);
+        #ifdef BACKLIGHT_ENABLE
+          backlight_step();
+        #endif
+        #ifdef RGBLIGHT_ENABLE
+          rgblight_step();
+        #endif
+        #ifdef __AVR__
+        writePinLow(E6);
+        #endif
+      } else {
+        unregister_code(KC_RSFT);
+        #ifdef __AVR__
+        writePinHigh(E6);
+        #endif
+      }
+      return false;
+  }
     return true;
 };
 
