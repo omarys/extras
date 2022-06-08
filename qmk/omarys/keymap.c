15b01fe71a73508b9b36dbf5fccd1772e5cb2c29
Scott O'Mary <omaryscott@gmail.com>
2022-06-08 09:59:48 -0500

    Updated FN button to jump to layer 3 in qmk keymap.

--- qmk/omarys/keymap.c
+++ qmk/omarys/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_QUOT,
   KC_ESC,  KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_LGUI, KC_LALT, MO(1), LOWER, KC_SPC, KC_SPC, TT(2), KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR
+  KC_LCTL, KC_LGUI, KC_LALT, MO(3), LOWER, KC_SPC, KC_SPC, TT(2), KC_MUTE, KC_VOLD, KC_VOLU, KC_PSCR
 ),
 
 /* Lower
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,   KC_NO,   KC_NO,   KC_UP,   KC_NO,   KC_NO,  KC_NO, KC_NO,    KC_7,    KC_8,    KC_9,    KC_PPLS,
   KC_TRNS, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_G,   KC_NO, KC_NO,    KC_4,    KC_5,    KC_6,    KC_PPLS,
   KC_NO,   KC_NO,   KC_X,    KC_NO,   KC_NO,   KC_NO,  KC_NO, KC_NO,    KC_1,    KC_2,    KC_3,    KC_ENT,
-  KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_TRNS, KC_SPC, KC_SPC, KC_TRNS, KC_0,    KC_DOT,  KC_CALC, KC_ENT
+  KC_LCTL, KC_LGUI, KC_LALT, MO(3),   KC_TRNS, KC_SPC, KC_SPC, KC_TRNS, KC_0,    KC_DOT,  KC_CALC, KC_ENT
 ),
 
 /* Adjust (Lower + Raise)
